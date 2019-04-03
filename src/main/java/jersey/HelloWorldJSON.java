68ed1293d24b9008d73354f50fe0787b471ffe16
AndreasBGJensen <bgandreas@hotmail.com>
2019-04-03 14:24:24 +0200

    @POST

--- src/main/java/jersey/HelloWorldJSON.java
+++ src/main/java/jersey/HelloWorldJSON.java
@@ -3,13 +3,14 @@ package jersey;
 
 import com.google.gson.Gson;
 
-import javax.ws.rs.DefaultValue;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.QueryParam;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 import java.util.LinkedList;
+
+import static javafx.application.ConditionalFeature.MEDIA;
 
 
 @Path("/json")
@@ -20,7 +21,7 @@ public class HelloWorldJSON {
     @GET
     public String getMessage(
             @DefaultValue("111")
-            @QueryParam("ball") Integer ballId, @Context UriInfo uriInfo){
+            @QueryParam("ball") Integer ballId, @Context UriInfo uriInfo) {
 
         ballId2++;
         ballId2 = ballId2 == 2 ? 0 : ballId2;
@@ -28,9 +29,29 @@ public class HelloWorldJSON {
         System.out.println(ballId);
         Gson json = new Gson();
         Ball[] ball = new Ball[2];
-        ball[0] = new Ball(10, 20, 10,50);
-        ball[1] = new Ball(15, 5, 20,50);
+        ball[0] = new Ball(10, 20, 10, 50);
+        ball[1] = new Ball(15, 5, 20, 50);
         return json.toJson(ball[ballId2]); //ball.toString();
     }
 
+    @POST
+
+    //@Consumes(MediaType.TEXT_XML)
+    public String postStrMsg(String msg) {
+        String output = "POST:Jersey say : " + msg;
+        return output;
+    }
+
+
+   /* @POST
+
+    @Consumes(MediaType.APPLICATION_JSON)
+    public String postBall(String str) {
+
+
+
+    }*/
+
 }
+
+
