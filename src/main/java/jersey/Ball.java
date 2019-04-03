35ec0e68bfe4a37cbd1215b27abc8cdbcd09a592
maltebp <maltebp94@gmail.com>
2019-04-03 12:37:51 +0200

    Testing static java

--- src/main/java/jersey/Ball.java
+++ src/main/java/jersey/Ball.java
@@ -3,9 +3,17 @@ package jersey;
 public class Ball {
     double x;
     double y;
+    double xVel;
+    double yVel;
 
-    public Ball(double x, double y){
+    public Ball(double x, double y, double xVel, double yVel){
         this.x = x;
         this.y = y;
+        this.xVel = xVel;
+        this.yVel = yVel;
+    }
+
+    public String toString(){
+        return "{ \"x\":\""+x+", \"y\":\""+y+"\"}";
     }
 }
--- src/main/java/jersey/HelloWorldJSON.java
+++ src/main/java/jersey/HelloWorldJSON.java
@@ -1,25 +1,36 @@
 package jersey;
 
+
+import com.google.gson.Gson;
+
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import java.util.LinkedList;
 
-import org.json.*;
 
 @Path("/json")
 public class HelloWorldJSON {
 
+    static int ballId2 = 0;
 
     @GET
-    public String getMessage(){
-        //Ball ball = new Ball(10, 20);
+    public String getMessage(
+            @DefaultValue("111")
+            @QueryParam("ball") Integer ballId, @Context UriInfo uriInfo){
 
-        JSONObject json = new JSONObject();
+        ballId2++;
+        ballId2 = ballId2 == 2 ? 0 : ballId2;
 
-        json.put("msg", "hello world");
-
-        return json.toString();
+        System.out.println(ballId);
+        Gson json = new Gson();
+        Ball[] ball = new Ball[2];
+        ball[0] = new Ball(10, 20, 10,50);
+        ball[1] = new Ball(15, 5, 20,50);
+        return json.toJson(ball[ballId2]); //ball.toString();
     }
-
-
 
 }
