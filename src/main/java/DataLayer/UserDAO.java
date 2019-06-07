d9e0874d9dbf868e9cccbc5304f71ae48163ba69
Simon Woldbye-Lyng <swoldbye@gmail.com>
2019-06-07 09:46:32 +0200

    Some documentation.

--- src/main/java/DataLayer/UserDAO.java
+++ src/main/java/DataLayer/UserDAO.java
@@ -8,12 +8,20 @@ public class UserDAO implements IUserDAO{
 
 
     //The following 3 functions are dataLayer Accessors.
+
+    /**
+     * createConnection() is a helper-function that creates an open connection to the mySQL server.
+     * NOTE: "com.mysql.jdbc.Driver" selects the driver to use to connect.
+     * @return Connection
+     * @throws SQLException
+     */
     @Override
     public Connection createConnection() throws SQLException {
         String dbUrl = "jdbc:mysql://ec2-52-30-211-3.eu-west-1.compute.amazonaws.com/s180943?";
         String dbUsername = "s180943";
         String dbPassword = "UXZTadQzbPrlIosGCZYNF";
         try{
+            //this specifies the driver for tomcat to use to communicate to mySQL server
             Class.forName("com.mysql.jdbc.Driver");
             return DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
         }catch(ClassNotFoundException e){
