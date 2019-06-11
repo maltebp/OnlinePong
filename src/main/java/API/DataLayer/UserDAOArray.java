92f29f90f5f92db3aab70de37b01f578c5d50924
AndreasBGJensen <bgandreas@hotmail.com>
2019-06-11 14:14:02 +0200

    UserDAOArrayliste

--- src/main/java/API/DataLayer/UserDAOArray.java
+++ src/main/java/API/DataLayer/UserDAOArray.java
@@ -1,39 +1,80 @@
 package API.DataLayer;
-/*
+
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
-public class UserDAOArray {
+public class UserDAOArray implements IUserDAO {
+
+    ArrayList<IUserDTO> userList;
+
+    @Override
+    public Connection createConnection() throws SQLException {
+
+        for(int i = 0; i<10; i++){
+            userList.add(new UserDTO(i, "Test"+i));
+        }
+
+        return null;
+    }
+
+    @Override
+    public IUserDTO getDBUser(int id) throws DALException {
+       IUserDTO user = searchUser(id);
+           if(user!=null){
+
+               return user;
+
+           }else{
+                System.out.println("User does not Exist");
+
+        }
+        return null;
+    }
+
+    @Override
+    public IUserDTO makeUser(ResultSet set) throws SQLException {
+        return null;
+    }
 
 
-    Connection createConnection() throws SQLException{}
+    @Override
+    public IUserDTO getDBScore(IUserDTO user) throws SQLException {
+        return user;
 
 
+    }
+
+    @Override
+    public String newScore(int id, int score) throws SQLException {
+        IUserDTO user = searchUser(id);
+        user.addScore(score);
+
+        return "Score added to the user";
+    }
 
 
-    public UserDAO getDBUser(int id) throws IUserDAO.DALException{}
+    @Override
+    public boolean validateUserByName(String username) throws SQLException {
+        return false;
+    }
 
+    @Override
+    public String createUser(String username, String password) throws SQLException {
+        IUserDTO user = new UserDTO(userList.size()+1,username);
+        user.setPassword(password);
+        userList.add(user);
 
-    IUserDTO makeUser(ResultSet set) throws SQLException;
+        return "User is created";
+    }
 
-    IUserDTO getDBScore(IUserDTO user) throws SQLException;
-
-    String newScore(int id, int score)throws SQLException;
-
-    boolean validateUserByName(String username) throws SQLException;
-
-    String createUser(String username, String password) throws SQLException;
-
-
-
-    class DALException extends Exception {
-        //Til Java serialisering...
-        private static final long serialVersionUID = 7355418246336739229L;
-
-        public DALException(String msg, Throwable e) {super(msg,e);}
-        public DALException(String msg) { super(msg);}
+    private IUserDTO searchUser(int id) {
+        for (int i = 0; i < userList.size(); i++) {
+            if (userList.get(i).getUserId() == i) {
+                return userList.get(i);
+            }
+        }
+        return null;
     }
 }
-}
-*/
