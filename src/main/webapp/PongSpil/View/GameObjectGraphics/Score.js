f190d59d75d9c6ed1a817725b714b12fc57c5bbf
Kristian Andersen <kristian1711@gmail.com>
2019-06-11 13:15:16 +0200

    Comments to the full code of the Pong game (final, had forgotten a bit when Stig came for conversation)

--- src/main/webapp/PongSpil/View/GameObjectGraphics/Score.js
+++ src/main/webapp/PongSpil/View/GameObjectGraphics/Score.js
@@ -17,17 +17,23 @@ function Score(x,y, score) {
     this.score = score;
 }
 
+/**
+ * Increments the players score by one, when a goal is scored
+ */
+Score.prototype.goal = function() {
+    this.score++;
+    if(this.score === maxScore) {
+        endGame();
+    }
+};
+
+/**
+ * Rendering function
+ */
 Score.prototype.render = function() {
     context.beginPath();
     context.font = "30px Arial";
     context.fillStyle = "#FFFFFF";
     context.fillText(""+ this.score, this.x, this.y);
     context.closePath();
-};
-
-Score.prototype.goal = function() {
-    this.score++;
-    if(this.score === maxScore) {
-        endGame();
-    }
 };
