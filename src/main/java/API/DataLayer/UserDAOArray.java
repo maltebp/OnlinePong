9a4edcf2c0cfae62c57f03c8c352eabb30940b6b
AndreasBGJensen <bgandreas@hotmail.com>
2019-06-11 13:43:06 +0200

    UserDAOArray

---
+++ src/main/java/API/DataLayer/UserDAOArray.java
@@ -0,0 +1,39 @@
+package API.DataLayer;
+/*
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserDAOArray {
+
+
+    Connection createConnection() throws SQLException{}
+
+
+
+
+    public UserDAO getDBUser(int id) throws IUserDAO.DALException{}
+
+
+    IUserDTO makeUser(ResultSet set) throws SQLException;
+
+    IUserDTO getDBScore(IUserDTO user) throws SQLException;
+
+    String newScore(int id, int score)throws SQLException;
+
+    boolean validateUserByName(String username) throws SQLException;
+
+    String createUser(String username, String password) throws SQLException;
+
+
+
+    class DALException extends Exception {
+        //Til Java serialisering...
+        private static final long serialVersionUID = 7355418246336739229L;
+
+        public DALException(String msg, Throwable e) {super(msg,e);}
+        public DALException(String msg) { super(msg);}
+    }
+}
+}
+*/
