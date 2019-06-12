e6cc016c5fc5f1fdf4de8892e7adbf50a0c652b5
JRJ86 <42966158+JRJ86@users.noreply.github.com>
2019-06-12 10:29:14 +0200

    mindre hotfixes hvilke inkluderer:
    -  ball_bottom til ball_top da der var en misforståelse omkring hvor elementerne bliver renderet fra
    
    -  Lavet StartBtn til startButton
    
    -  Andre små ubetydelige comment fixes

--- src/main/webapp/PongSpil/GlobalVariables.js
+++ src/main/webapp/PongSpil/GlobalVariables.js
@@ -8,6 +8,6 @@ var player1, player2, ball;                     //Paddle and Ball objects are cr
 
 //Variables
 var keysDown = {83: false, 87: false};                  //Keys 'S' and 'W', which respectively makes the paddle go 'down' or 'up'
-var startBtn = document.getElementById("startBtn");     //Reference to the 'Start Button'
+var startButton = document.getElementById("startBtn");     //Reference to the 'Start Button'
 var maxScore;                                           //Score the game is running 'till
 var gameRunning;                                        //Boolean to control a game is being played
