af5f404a1567fa1c045438491945b361b3479a84
maltebp <maltebp94@gmail.com>
2019-06-14 12:15:00 +0200

    Game Server: Created tests for EloAlgorithm and they work perfectly

--- src/main/java/gameserver/control/EloAlgorithm.java
+++ src/main/java/gameserver/control/EloAlgorithm.java
@@ -5,20 +5,25 @@ public class EloAlgorithm implements RatingAlgorithm {
     private static final double ELO_K = 30; //The maximum a value may change
 
     @Override
-    public int calculateLoserChange(double winnerRating, double loserRating) {
+    public int calculateLoserChange(int winnerRating, int loserRating) {
         double loserChance = chanceToWin(loserRating, winnerRating);
-        System.out.println("Loser: "+loserChance);
+        //System.out.println("Loser: "+loserChance);
         double ratingChange =  ELO_K * ( 0 - loserChance );
-        System.out.println(ratingChange);
-        return (int) Math.ceil(ratingChange);
+        //System.out.println(ratingChange);
+        if( loserRating+ratingChange < 0 ){
+            return -loserRating;
+        }
+
+        return (int) Math.floor(ratingChange);
     }
 
+
     @Override
-    public int calculateWinnerChange(double winnerRating, double loserRating) {
+    public int calculateWinnerChange(int winnerRating, int loserRating) {
         double winnerChance = chanceToWin(winnerRating, loserRating);
-        System.out.println("Winner: "+winnerChance);
+        //System.out.println("Winner: "+winnerChance);
         double ratingChange = ELO_K * ( 1 - winnerChance );
-        System.out.println(ratingChange);
+        //System.out.println(ratingChange);
         return (int) Math.ceil(ratingChange);
     }
 
--- src/main/java/gameserver/control/RatingAlgorithm.java
+++ src/main/java/gameserver/control/RatingAlgorithm.java
@@ -7,13 +7,13 @@ public interface RatingAlgorithm {
      * @param loserRating Rating of the loser of a match before a match is started
      * @return The change to the losers's rating
      */
-    int calculateLoserChange( double winnerRating, double loserRating );
+    int calculateLoserChange( int winnerRating, int loserRating );
 
     /**
      * @param winnerRating Rating of the winner of a match, before a match is started
      * @param loserRating Rating of the loser of a match before a match is started
      * @return The change to the winner's rating
      */
-    int calculateWinnerChange( double winnerRating, double loserRating );
+    int calculateWinnerChange( int winnerRating, int loserRating );
 
 }
