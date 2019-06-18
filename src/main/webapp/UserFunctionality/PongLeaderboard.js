2c047dc40001a411dda940d2ff847adb508a9179
maltebp <maltebp94@gmail.com>
2019-06-18 19:25:33 +0200

    Added loading spinner to leaderboard

--- src/main/webapp/UserFunctionality/PongLeaderboard.js
+++ src/main/webapp/UserFunctionality/PongLeaderboard.js
@@ -1,6 +1,19 @@
 
-$(document).ready(function () {
+
+var leaderboardLoader = document.getElementById("leaderboardLoader");
+var leaderboard = document.getElementById("leaderboard");
+
+leaderboard.style.display = "none";
+leaderboardLoader.style.display = "none";
+
+
+function loadLeaderboard(){
+
+    leaderboardLoader.style.display = "inline";
+
     apiGet( "/getTopTen", function(data) {
+        leaderboardLoader.style.display = "none";
+        leaderboard.style.display = "inline";
         var userData = "";
         $.each(data, function (key, value) {
             userData += '<tr>';
@@ -10,4 +23,8 @@ $(document).ready(function () {
         });
         $('#topTen').append(userData);
     });
+}
+
+$(document).ready(function () {
+    loadLeaderboard();
 });
