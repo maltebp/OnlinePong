2f74ca6cf4cfcf734c6f597faeb9b557665ba3ae
maltebp <maltebp94@gmail.com>
2019-06-18 20:14:08 +0200

    Cleaned up user registration page

--- src/main/webapp/UserFunctionality/UserRegistration.js
+++ src/main/webapp/UserFunctionality/UserRegistration.js
@@ -2,35 +2,89 @@
  * Class to control the registration HTML of the website
  */
 
+var registerLayer = document.getElementById("registerLayer");
+var registerLoading = document.getElementById("loadingLayer");
+var registerError = document.getElementById("registerErrorMsg");
+var registerSuccess = document.getElementById("successLayer");
+
+var registrationUsername = "";
+var registrationPassword = "";
+
+registerError.style.display = "none";
+registerSuccess.style.display = "none";
+
+
+
+function evaluateResponse(result){
+
+    toggleLoader(false);
+
+    switch(result.code){
+
+        case "1":
+            currUser = registrationUsername;
+            currPassw = registrationPassword;
+            registerLayer.style.display = "none";
+            registerSuccess.style.display = "inline";
+            break;
+
+        default:
+            registerLayer.style.display = "inline";
+            showError("Something went wrong. User wasn't created.");
+
+        registrationUsername = "";
+        registrationPassword = "";
+    }
+}
+
+
+
+function showError(msg){
+    registerError.innerHTML = msg;
+    registerError.style.display = "inline";
+}
+
+
+function toggleLoader(toggle){
+    if(toggle){
+        registerLayer.style.display = "none";
+        registerLoading.style.display = "inline";
+    }else{
+        registerLayer.style.display = "inline";
+        registerLoading.style.display = "none";
+    }
+}
+
+
 function isSamePasswords() {
     var pass = document.getElementById("password").value;
     var pass2 = document.getElementById("passwConf").value;
     return (pass === pass2);
 }
 
+function createUser(){
 
-function evaluateResponse(result){
-    switch(result.code){
+    if( isSamePasswords() ) {
+        var userObj = $('#form').serializeJSON();
+        delete userObj.passwConf;
 
-        case "1":
-            alert("User created!");
-            break;
+        toggleLoader(true);
 
-        default:
-            alert("Something went wrong" + JSON.stringify(result))
+        registrationUsername = userObj.username;
+        registrationPassword = userObj.password;
+        apiPost("/createUser", evaluateResponse, JSON.stringify(userObj));
+
+    } else {
+        showError("Passwords doesn't match");
     }
+
 }
 
 
 $('form').on('submit', function(){
-    if(isSamePasswords()) {
-        var userObj = $('#form').serializeJSON();
-        delete userObj.passwConf;
-
-        apiPost("/createUser", evaluateResponse, JSON.stringify(userObj));
-
-        return false;
-    } else {
-        document.getElementById("registerFailMsg").innerHTML = "Passwords didn't match";
-    }
+    createUser();
+    return false;
 });
+
+
+toggleLoader(false);
