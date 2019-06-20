1a5f3cc87b9bf36f4db56d68bd3f7fbcfe044b5e
Kristian Andersen <kristian1711@gmail.com>
2019-06-20 12:46:56 +0200

    leaderboard works again (after data recieved also has a code + description - no use implemented at webapp level yet)

--- src/main/webapp/userFunctionality/leaderboard/pongLeaderboard.js
+++ src/main/webapp/userFunctionality/leaderboard/pongLeaderboard.js
@@ -6,25 +6,22 @@ hide(leaderboard);
 hide(leaderboardLoader);
 
 function loadLeaderboard(){
-
     show(leaderboardLoader);
 
-    // apiGet( "/getTopTen", function(data) {
-    //     console.log(data);
-    //     hide(leaderboardLoader);
-    //     show(leaderboard);
-    //     // var jsonObj = new JSONObject(data);
-    //     // var users = data.getJSONArray("users");
-    //     // var leaderboardData = "";
-    //     $.each(data.users, function (key, value) {
-    //         if(value.username !== '')
-    //         leaderboardData += '<tr>';
-    //         leaderboardData += '<td>'+value.username+'</td>';
-    //         leaderboardData += '<td>'+value.elo+'</td>';
-    //         leaderboardData += '</tr>';
-    //     });
-    //     $('#topTen').append(leaderboardData);
-    // });
+    apiGet( "/getTopTen", function(data) {
+        console.log(data);
+        hide(leaderboardLoader);
+        show(leaderboard);
+        var leaderboardData = "";
+        $.each(data.users, function (key, value) {
+            if(value.username !== '')
+            leaderboardData += '<tr>';
+            leaderboardData += '<td>'+value.username+'</td>';
+            leaderboardData += '<td>'+value.elo+'</td>';
+            leaderboardData += '</tr>';
+        });
+        $('#topTen').append(leaderboardData);
+    });
 }
 
 $(document).ready(function () {
