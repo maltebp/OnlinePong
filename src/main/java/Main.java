b16ec53ae8530b177fe393994e49f7ab610c8eef
maltebp <maltebp94@gmail.com>
2019-06-20 15:00:58 +0200

    Changed MySQL db to default

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -20,7 +20,7 @@ import org.apache.catalina.startup.Tomcat;
  */
 public class Main {
 
-    private static final boolean USE_ARRAY_DB = true;
+    private static final boolean USE_ARRAY_DB = false;
 
     public static void main(String[] args) throws LifecycleException  {
 
